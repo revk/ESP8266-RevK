d4d4822831ed2d00a0ab9974e77289b800f39d44
Adrian Kennard <adrian@158.176.240.91.in-addr.arpa>
2019-04-17 07:33:01 +0100

    Making progress

--- src/ESP8266RevK.cpp
+++ src/ESP8266RevK.cpp
@@ -19,119 +19,176 @@
 #endif
 
 #include <ESP8266RevK.h>
-
 #include <ESP8266httpUpdate.h>
 
-static const char *appname=NULL;
-static const char *firmware="excalibur.bec.aa.net.uk";
+// Local functions
+static void pub (const char *prefix, const char *suffix, const char *fmt, ...);
 
-void	upgrade(const byte * message, size_t len)
+// Statics rather than in class - the mqtt callback was a problem, and really this is system wide stuff
+static const char *appname = NULL;      // System set from constructor as literal string
+
+// Settings that can be changed
+static char firmware[129] = "excalibur.bec.aa.net.uk";  // Host from which we load new code
+
+static char hostname[33] = { }; // Unique ID of host (default is chipid)
+
+static char wifissid[33] = "IoT";       // WiFi SSID
+static char wifipass[33] = "security";  // WiFi Password
+static char mqtthost[129] = "mqtt.iot"; // MQTT hostname/IP
+static char mqttuser[33] = "";  // MQTT usernmame
+static char mqttpass[33] = "";  // MQTT password
+
+// Local variables
+static WiFiClient mqttclient;
+static PubSubClient mqtt;
+static unsigned long mqttping = 0;
+static unsigned long mqttretry = 0;
+static unsigned long mqttbackoff = 100;
+static unsigned short mqttport = 1883;
+
+void
+upgrade (const byte * message, size_t len)
 {
-	delay(1000);	// Allow clean MQTT
-	char		url       [200];
-	snprintf(url, sizeof(url), "/%s.ino." BOARD ".bin", appname);
-	WiFiClient	client;
-	if (ESPhttpUpdate.update(client, firmware, 80, url)) {
-		Serial.println(ESPhttpUpdate.getLastErrorString());
-	}
-	delay(1000);
+   char url[200];
+   snprintf (url, sizeof (url), "/%s.ino." BOARD ".bin", appname);
+   pub ("stat", "upgrade", "Upgrading from http://%s/%s", firmware, url);
+   delay (1000);                // Allow clean MQTT report/ack
+   WiFiClient client;
+   if (ESPhttpUpdate.update (client, firmware, 80, url))
+      Serial.println (ESPhttpUpdate.getLastErrorString ());
+   delay (1000);
 }
 
-static void		message(const char *topic, byte * payload, unsigned int len)
+static void
+message (const char *topic, byte * payload, unsigned int len)
 {
-	char           *p = strchr(topic, '/');
-	if (!p)
-		return;
-	char           *prefix = (char *)alloca(p - topic + 1);
-	strncpy(prefix, topic, p - topic);
-	prefix[p - topic] = 0;
-	p = strrchr(p + 1, '/');
-	if (p)
-		p++;
-	else
-		p = NULL;
-	if (!strcmp(prefix, "cmnd") && p && !strcmp(p, "upgrade")) {
-		/* Do upgrade from web */
-		upgrade(payload, len);
-		return;
-		/* Yeh, would not get here. */
-	}
-	app_mqtt(prefix,p,payload,len);
+   char *p = strchr (topic, '/');
+   if (!p)
+      return;
+   char *prefix = (char *) alloca (p - topic + 1);
+   strncpy (prefix, topic, p - topic);
+   prefix[p - topic] = 0;
+   p = strrchr (p + 1, '/');
+   if (p)
+      p++;
+   else
+      p = NULL;
+   if (!strcmp (prefix, "cmnd") && p)
+   {
+      if (!strcmp (p, "upgrade"))
+      {
+         /* Do upgrade from web */
+         upgrade (payload, len);
+         return;
+         /* Yeh, would not get here. */
+      }
+      if (app_cmnd (p, payload, len))
+         return;
+      pub ("stat", "error", "Unknown command");
+      return;
+   }
 }
 
-ESP8266RevK::ESP8266RevK(const char *myappname)
+ESP8266RevK::ESP8266RevK (const char *myappname)
 {
-	appname=myappname;
-	if (!*hostname)
-		snprintf(hostname, sizeof(hostname), "%06X", ESP.getChipId());
-	if (!*wifissid)
-		strncpy(wifissid, "IoT", sizeof(wifissid));
-	if (!*wifipass)
-		strncpy(wifipass, "security", sizeof(wifipass));
-	if (!*mqtthost)
-		strncpy(mqtthost, "mqtt.iot", sizeof(mqtthost));
-	WiFi.hostname(hostname);
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(wifissid, wifipass);
-	if(*mqtthost)
-	{
-		mqtt=PubSubClient();
-		mqtt.setClient(mqttclient);
-		mqtt.setServer(mqtthost, mqttport);
-		mqtt.setCallback(message);
-	}
+   appname = myappname;
+   if (!*hostname)
+      snprintf (hostname, sizeof (hostname), "%06X", ESP.getChipId ());
+   if (!*wifissid)
+      strncpy (wifissid, "IoT", sizeof (wifissid));
+   if (!*wifipass)
+      strncpy (wifipass, "security", sizeof (wifipass));
+   if (!*mqtthost)
+      strncpy (mqtthost, "mqtt.iot", sizeof (mqtthost));
+   WiFi.hostname (hostname);
+   WiFi.mode (WIFI_STA);
+   WiFi.begin (wifissid, wifipass);
+   if (*mqtthost)
+   {
+      mqtt = PubSubClient ();
+      mqtt.setClient (mqttclient);
+      mqtt.setServer (mqtthost, mqttport);
+      mqtt.setCallback (message);
+   }
 }
 
-void		ESP8266RevK::loop()
+void
+ESP8266RevK::loop ()
 {
-	/* MQTT reconnnect */
-	if (*mqtthost&&!mqtt.loop() && mqttretry < millis()) {
-		char topic[101];
-		snprintf(topic, sizeof(topic), "tele/%s/%s/LWT", appname, hostname);
-		if (mqtt.connect(hostname, mqttuser, mqttpass,topic,MQTTQOS1,true,"Offline")) {
-			/* Worked */
-			mqttbackoff = 1000;
-			mqtt.publish(topic,"Online",true);
-			snprintf(topic, sizeof(topic), "+/%s/%s/#", appname, hostname);
-			/* Specific device */
-			mqtt.subscribe(topic);
-			/* All devices */
-			snprintf(topic, sizeof(topic), "+/%s/*/#", appname);
-			mqtt.subscribe(topic);
-		} else if (mqttbackoff < 300000)
-			mqttbackoff *= 2;
-		mqttretry = millis() + mqttbackoff;
-		/*Serial.printf("MQTT connnect %s\n",mqtthost);*/
-	}
+   /* MQTT reconnnect */
+   if (*mqtthost && !mqtt.loop () && mqttretry < millis ())
+   {
+      char topic[101];
+      snprintf (topic, sizeof (topic), "tele/%s/%s/LWT", appname, hostname);
+      if (mqtt.connect (hostname, mqttuser, mqttpass, topic, MQTTQOS1, true, "Offline"))
+      {
+         /* Worked */
+         mqttbackoff = 1000;
+         mqtt.publish (topic, "Online", true);
+         snprintf (topic, sizeof (topic), "+/%s/%s/#", appname, hostname);
+         /* Specific device */
+         mqtt.subscribe (topic);
+         /* All devices */
+         snprintf (topic, sizeof (topic), "+/%s/*/#", appname);
+         mqtt.subscribe (topic);
+      } else if (mqttbackoff < 300000)
+         mqttbackoff *= 2;
+      mqttretry = millis () + mqttbackoff;
+   }
 }
 
-void		ESP8266RevK::pub(const char *prefix, const char *suffix, const char *fmt,...)
+static void
+pubap (const char *prefix, const char *suffix, const char *fmt, va_list ap)
 {
-	if(!*mqtthost)return;
-	char		temp      [256]={};
-	if(fmt)
-	{
-		va_list		ap;
-		va_start(ap, fmt);
-		vsnprintf(temp, sizeof(temp), fmt, ap);
-		va_end(ap);
-	}
-	char		topic     [101];
-	snprintf(topic, sizeof(topic), "%s/%s/%s/%s", prefix, appname, hostname, suffix);
-	mqtt.publish(topic, temp);
+   if (!*mqtthost)
+      return;
+   char temp[256] = { };
+   if (fmt)
+      vsnprintf (temp, sizeof (temp), fmt, ap);
+   char topic[101];
+   snprintf (topic, sizeof (topic), "%s/%s/%s/%s", prefix, appname, hostname, suffix);
+   mqtt.publish (topic, temp);
 }
 
-void start_setting()
-{        // Store settings in EEPROM
- // TODO
+static void
+pub (const char *prefix, const char *suffix, const char *fmt, ...)
+{
+   va_list ap;
+   va_start (ap, fmt);
+   pub (prefix, suffix, fmt, ap);
+   va_end (ap);
 }
 
-void add_setting(const char *name,const char*value)
+void
+ESP8266RevK::stat (const char *suffix, const char *fmt, ...)
 {
- // TODO
+   va_list ap;
+   va_start (ap, fmt);
+   pub ("stat", suffix, fmt, ap);
+   va_end (ap);
 }
 
-void end_setting()
+void
+ESP8266RevK::tele (const char *suffix, const char *fmt, ...)
 {
- // TODO
+   va_list ap;
+   va_start (ap, fmt);
+   pub ("tele", suffix, fmt, ap);
+   va_end (ap);
+}
+
+void
+ESP8266RevK::pub (const char *prefix, const char *suffix, const char *fmt, ...)
+{
+   va_list ap;
+   va_start (ap, fmt);
+   pub (prefix, suffix, fmt, ap);
+   va_end (ap);
+}
+
+
+void
+setting (const char *name, const char *value)
+{
+   // TODO
 }
--- src/ESP8266RevK.h
+++ src/ESP8266RevK.h
@@ -9,33 +9,21 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Functions expected in the app
-void app_mqtt(const char *prefix, const char*suffix, const byte *message, size_t len); // Called for incoming messages
-void app_setting(const char *setting,const char *value);	// Called for settings from EEPROM
+// Functions expected in the app (return true if OK)
+boolean app_cmnd(const char*suffix, const byte *message, size_t len); // Called for incoming commands not already handled
+boolean app_setting(const char *setting,const char *value);	// Called for settings from EEPROM
+
 
 class ESP8266RevK : private PubSubClient {
  public:
    ESP8266RevK(const char*myappname="RevK");
-   void loop(void);
-   void pub(const char *prefix, const char *suffix, const char *fmt=NULL, ...);
-   void start_setting();	// Store settings in EEPROM
-   void add_setting(const char *name,const char*value);
-   void end_setting();
-
-   char hostname[33];	// Unique ID of host (default is chipid)
-   char wifissid[33];	// WiFi SSID
-   char wifipass[33];	// WiFi Password
-   char mqtthost[129];	// MQTT hostname/IP
-   char mqttuser[33];	// MQTT usernmame
-   char mqttpass[33];	// MQTT password
+   void loop(void);	// Call in loop
+   void stat(const char *suffix, const char *fmt=NULL, ...); // Publish stat
+   void tele(const char *suffix, const char *fmt=NULL, ...); // Publish tele
+   void pub(const char *prefix, const char *suffix, const char *fmt=NULL, ...);	// Publish general
+   void setting(const char *name,const char*value);
 
  private:
-   WiFiClient mqttclient;
-   PubSubClient mqtt;
-   unsigned long mqttping = 0;
-   unsigned long mqttretry = 0;
-   unsigned long mqttbackoff = 100;
-   unsigned short mqttport = 1883;
 };
 
 #endif
