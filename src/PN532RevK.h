539b281e9b3b13320a21fe6834e173c46e3dd332
RevK <github@e.gg>
2019-05-21 17:10:28 +0100

    PN532 code

---
+++ src/PN532RevK.h
@@ -0,0 +1,23 @@
+#ifndef __PN532RevK_H__
+#define __PN532RevK_H__
+
+#include <PN532RevK.h>
+
+class PN532RevK
+{
+  public:
+    PN532RevK(PN532Interface &interface);
+    uint32_t begin(int timeout=100); // Start, get version (0=bad)
+    uint8_t cardsPresent(int timeout=100);	// return number of cards (0=error or none)
+    uint8_t inField (int timeout=100);		// return if in field (0=OK, else status)
+    uint8_t getID(String &id1,int timeout=100); // Get the ID (returns number of cards, i.e. 0 or 1)
+    uint8_t data(uint8_t txlen,uint8_t *tx,uint8_t &rxlen,uint8_t *rx,int timeout=100); // Exchange data, return 0 if OK (status), rx has status byte at start
+    uint8_t release(int timeout=100); // Release target
+    uint8_t target(int timeout=100); // Start as target
+
+  private:
+    PN532Interface *_interface;
+    uint8_t Tg1; // Tag ID
+};
+
+#endif
